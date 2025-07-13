e52af7949db0f51cba9046096b66b8d8d0afa196
AlexG345 <alexgernez1@gmail.com>
2025-07-14 00:36:50 +0200

    some menu changes

--- lua/weapons/gmod_tool/stools/adv_health_tool.lua
+++ lua/weapons/gmod_tool/stools/adv_health_tool.lua
@@ -19,9 +19,9 @@ TOOL.Information = {
 if CLIENT then
 
 	local t = "tool."..mode.."."
-	language.Add( t.."listname",	"Health Changer" )
-	language.Add( t.."name",		"Health Changer" )
-	language.Add( t.."desc",		"Change the health-related properties of an object" )
+	language.Add( t.."listname",	"Advanced Health" )
+	language.Add( t.."name",		"Advanced Health Tool" )
+	language.Add( t.."desc",		"Change the health-related properties of entities." )
 	language.Add( t.."left",		"Apply settings" )
 	language.Add( t.."right",		"Copy settings" )
 	language.Add( t.."reload",		"Reset entity settings" )
@@ -38,14 +38,6 @@ if SERVER then
 		end
 	end )
 
-	--[[
-	hook.Add( "PostEntityTakeDamage", "aht_burn_remover", function( target, dmginfo )
-		if target.aht_fire_immune then
-			target:Extinguish()
-		end
-	end )
-	]]
-
 end
 
 local function AHT_CopySettings( ent )
@@ -57,19 +49,12 @@ local function AHT_CopySettings( ent )
 	}
 end
 
-local function AHT_ApplyHealth( ent, health )
-	if not ( health and isfunction( ent.SetHealth ) ) then return false end
-	if not ent.aht_orig_health then ent.aht_orig_health = ent:Health() end
-	if ent.aht_orig_health == health then ent.aht_orig_health = nil end
-	ent:SetHealth( health )
-	return true
-end
 
-local function AHT_ApplyMaxHealth( ent, maxHealth )
-	if not ( maxHealth and isfunction( ent.SetMaxHealth ) ) then return end
-	if not ent.aht_orig_max_health then ent.aht_orig_max_health = ent:GetMaxHealth() end
-	if ent.aht_orig_max_health == maxHealth then ent.aht_orig_max_health = nil end
-	ent:SetMaxHealth( maxHealth )
+local function AHT_ApplyNRemember( ent, newval, setter, getter, orig_key )
+	if not ( newval and isfunction( setter ) ) then return false end
+	local o = ent[ orig_key ] or getter( ent )
+	ent[ orig_key ] = o ~= newval and o or nil
+	setter( ent, newval )
 	return true
 end
 
@@ -86,11 +71,12 @@ function AHT_ApplySettings( ply, ent, data, do_undo )
 		end
 	end
 
-	AHT_ApplyHealth( ent, data.health )
-	AHT_ApplyMaxHealth( ent, data.max_health )
-	ent.aht_unbreakable = data.unbreakable or nil
-	ent.aht_fire_immune = data.fire_immune or nil
-	ent.aht_damage_filtered = ent.aht_unbreakable or ent.aht_fire_immune or nil
+	AHT_ApplyNRemember( ent, data.health, ent.SetHealth, ent.Health, "aht_orig_health" )
+	AHT_ApplyNRemember( ent, data.max_health, ent.SetMaxHealth, ent.GetMaxHealth, "aht_orig_max_health" )
+	local k1, k2 = "unbreakable", "fire_immune"
+	ent["aht_"..k1] = data[k1] or nil
+	ent["aht_"..k2] = data[k2] or nil
+	ent.aht_damage_filtered = data[k1] or data[k2] or nil
 
 	if SERVER then
 		duplicator.StoreEntityModifier( ent, "adv_health_tool", data )
@@ -120,11 +106,8 @@ if SERVER then
 		if not IsValid( ent ) then return end 
 
 		for k, v in pairs( AHT_CopySettings( ent ) ) do
-			if isnumber( v ) then
-				ent:SetNW2Int( "aht_"..k, v )
-			elseif isbool( v ) then
-				ent:SetNW2Bool( "aht_"..k, v )
-			end
+			local funcKey = isbool( v ) and "SetNW2Bool" or "SetNW2Int"
+			ent[ funcKey ]( ent, "aht_"..k, v )
 		end
 
 	end
@@ -169,7 +152,7 @@ function TOOL:Reload( trace )
 
 	if ent:IsWorld() or not ent:IsValid() then return false end
 
-	duplicator.ClearEntityModifier( ent, "adv_health_tool" )
+	if SERVER then duplicator.ClearEntityModifier( ent, "adv_health_tool" ) end
 
 	AHT_ApplySettings( nil, ent, {
 		health		= ent.aht_orig_health,
@@ -206,32 +189,34 @@ if CLIENT then
 		local max_health = ent:GetNW2Int( "aht_max_health" )
 		local unbreak	= ent:GetNW2Bool( "aht_unbreakable" )
 		local fireImm	= ent:GetNW2Bool( "aht_fire_immune" )
-		local prop = max_health ~= 0 and math.Clamp( health / max_health, 0, 1 ) or 1
+		local prop = health / max_health or 1 -- lua can handle division by 0
+
 
 		local text1 = ("Health: %s / %s"):format( health or "N/A", max_health or "N/A" )
-		local text2 = (" (%s%%)"):format( math.Round( 100*prop, 1 ) or "N/A" )
-		local text3 = unbreak and fireImm and "Unbreakable, Fire Immune"  or unbreak and "Unbreakable" or fireImm and "Fire Immune" or nil
+		local text2 = max_health ~= 0 and (" (%s%%)"):format( math.Round( prop*100, 2 ) ) or "" -- better to hide -inf, nan and inf than show it to players i guess
+		-- this is bad
+		local text3 = unbreak and fireImm and "Unbreakable, Fire Immune"  or unbreak and "Unbreakable" or fireImm and "Fire Immune" or ""
+		local font = "GModWorldtip"
 
-		local col2	= HSVToColor( 100*prop, 0.65, 0.9 )
+		prop = math.Clamp( prop, 0, 3 )
+		local txcol	= HSVToColor( 100*prop, 0.65, 0.9 )
 		local bgcol = HSVToColor( 100*prop, 1, 0.1 )
-		bgcol.a = 220
-		local font  = "GModWorldtip"
+			bgcol.a = 220
 		local rad   = 8
-		
+
 		surface.SetFont( font )
 		local tw1, th1 = surface.GetTextSize( text1 )
 		local tw2, th2 = surface.GetTextSize( text2 )
-		local tw3, th3 = 0, 0
-		if text3 then tw3, th3 = surface.GetTextSize( text3 ) end
-		local tw = math.max( tw1 + tw2, tw3 )
-		local th = th1 + th3
-		
+		local tw3, th3 = surface.GetTextSize( text3 )
+		if tw3 == 0 then th3 = 0 end
+		local tw, th = math.max( tw1 + tw2, tw3 ), th1 + th3
+
 		y = y - th
 
 		draw.RoundedBox( rad, x - tw/2 - 10, y - th1/2 - 2, tw + 20, th + 4, bgcol )
 		draw.SimpleText( text1, font, x - tw2/2, y, color_white, 1, 1 )
-		draw.SimpleText( text2, font, x + tw1/2, y, col2, 1, 1 )
-		if text3 then draw.SimpleText( text3, font, x, y+th1, color_gold, 1, 1 ) end
+		draw.SimpleText( text2, font, x + tw1/2, y, txcol, 1, 1 )
+		if text3 ~= "" then draw.SimpleText( text3, font, x, y+th1, color_gold, 1, 1 ) end
 	end
 
 end
@@ -242,11 +227,10 @@ local cvarlist = TOOL:BuildConVarList()
 
 function TOOL.BuildCPanel( cPanel )
 
-	local color_blue = Color( 50, 100, 200 )
-	local color_green = Color( 35, 155, 100 )
-	local color_gray = Color( 240, 240, 240 )
-	local color_orange = Color( 220, 120, 20 )
-	local color_red = Color( 220, 40, 20 )
+	local color_gray	= Color( 240, 240, 240 )
+	local color_red		= Color( 220, 40, 20 )
+	local color_orange	= Color( 220, 120, 20 )
+	local color_green	= Color( 35, 155, 100 )
 
 	local function paint( panel, w, h, hcol, bgcol )
 		local topHeight = panel:GetHeaderHeight()
@@ -259,8 +243,8 @@ function TOOL.BuildCPanel( cPanel )
 
 	cPanel:ToolPresets( mode, cvarlist )
 
-	local maxHealth = 2147483520
-	local lowHealth = 1
+	local limitHealth	= 2147483520
+	local lowHealth		= 1
 
 	local healthForm = vgui.Create( "DForm", cPanel )
 		cPanel:AddItem( healthForm )
@@ -268,23 +252,26 @@ function TOOL.BuildCPanel( cPanel )
 		healthForm:SetLabel( "Health and Max Health" )
 		healthForm:SetPaintBackground( false )
 		healthForm:DockPadding( 0, 0, 0, 5 )
-		function healthForm:Paint(w, h)
+		function healthForm:Paint( w, h )
 			paint( self, w, h, color_red, color_gray )
 		end
 
-		local maxHealthSlider = healthForm:NumSlider( "Max Health", mode.."_max_health", 0, maxHealth, 0 )
+		local maxHealthSlider = healthForm:NumSlider( "Max Health", mode.."_max_health", 0, 5000, 0 )
 			maxHealthSlider:SetToolTip("Sets the entity's maximum health. NPCs can heal up to this amount.")
+			healthForm:ControlHelp( "I recommend using a value greather than that of Base Health to prevent weird behavior for NPCs." )
 
-		local healthSlider = healthForm:NumSlider( "Base Health", mode.."_health", 0, maxHealth, 0 )
+		local healthSlider = healthForm:NumSlider( "Base Health", mode.."_health", 0, maxHealthSlider:GetMax(), 0 )
 			healthSlider:SetToolTip("Sets the current and duped health of the entity.")
+
+		healthForm:ControlHelp( ("You can set these higher than %s if you want."):format( maxHealthSlider:GetMax() ) )
 
 		local FR_button = vgui.Create( "DButton" )
 			FR_button:Dock( TOP )
 			FR_button:SetText( "Make fragile" )	
 			FR_button:SetImage( "icon32/hand_property.png" )
 			function FR_button:DoClick()
-				maxHealthSlider:SetValue( lowHealth )
-				healthSlider:SetValue( lowHealth )
+				maxHealthSlider.Scratch:SetValue( lowHealth )
+				healthSlider.Scratch:SetValue( lowHealth )
 			end
 			FR_button:SetToolTip( ("Set Base Health and Max Health to %s."):format( lowHealth ) )
 
@@ -293,24 +280,24 @@ function TOOL.BuildCPanel( cPanel )
 			NU_button:SetText( "Make near-unbreakable" )	
 			NU_button:SetImage( "icon32/tool.png" )
 			function NU_button:DoClick()
-				maxHealthSlider:SetValue( maxHealth )
-				healthSlider:SetValue( maxHealth )
+				maxHealthSlider.Scratch:SetValue( limitHealth )
+				healthSlider.Scratch:SetValue( limitHealth )
 			end
 			NU_button:DockMargin( 15, 0, 0, 0 )
-			NU_button:SetToolTip( ("Set Base Health and Max Health to %s."):format( maxHealth ) )
+			NU_button:SetToolTip( ("Set Base Health and Max Health to %s."):format( limitHealth ) )
 		
 		healthForm:AddItem( FR_button, NU_button )
 
-		local STMH_checkBox = healthForm:CheckBox( "Fully healed", mode.."_use_max" )
+		local STMH_checkBox = healthForm:CheckBox( "Heal", mode.."_use_max" )
 			STMH_checkBox:SetToolTip( ( "Set Base Health to the same value as Max Health" ) )
 			function STMH_checkBox:OnChange( checked )
 				healthSlider:SetEnabled( not checked )
-				if checked then healthSlider:SetValue( maxHealthSlider:GetValue() ) end
+				if checked then healthSlider.Scratch:SetValue( maxHealthSlider.Scratch:GetFloatValue() ) end
 			end
 
 		function maxHealthSlider:OnValueChanged( value )
 			if STMH_checkBox:GetChecked() then
-				healthSlider:SetValue( value )
+				healthSlider.Scratch:SetValue( maxHealthSlider.Scratch:GetFloatValue() )
 			end
 		end
 
@@ -320,7 +307,7 @@ function TOOL.BuildCPanel( cPanel )
 		filterForm:SetLabel( "Damage filtering" )
 		filterForm:SetPaintBackground( false )
 		filterForm:DockPadding( 0, 0, 0, 5 )
-		function filterForm:Paint(w, h)
+		function filterForm:Paint( w, h )
 			paint( self, w, h, color_orange, color_gray )
 		end
 
@@ -336,12 +323,12 @@ function TOOL.BuildCPanel( cPanel )
 		helpForm:SetLabel( "Help" )
 		helpForm:SetPaintBackground( false )
 		helpForm:DockPadding( 0, 0, 0, 5 )
-		function helpForm:Paint(w, h)
+		function helpForm:Paint( w, h )
 			paint( self, w, h, color_green, color_gray )
 		end
 
 		local tooltipCheckBox = helpForm:CheckBox( "Enable Tooltips", mode.."_tooltip_enabled" )
-			tooltipCheckBox:SetToolTip( "Show a health tooltip when pointing something with the tool." )
+			tooltipCheckBox:SetToolTip( "Show a health tooltip when looking at something with this tool." )
 
 
 end
