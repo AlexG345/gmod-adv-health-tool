643eac2ca808363b6ca49c10f360335defe0890d
AlexG345 <alexgernez1@gmail.com>
2025-07-29 20:49:32 +0200

    not much

--- lua/weapons/gmod_tool/stools/adv_health_tool.lua
+++ lua/weapons/gmod_tool/stools/adv_health_tool.lua
@@ -1,7 +1,7 @@
 local mode = TOOL.Mode -- Class name of the tool. (name of the .lua file) 
 
 TOOL.Category		= "Construction"
-TOOL.Name			= "#Tool."..mode..".listname"
+TOOL.Name			= "#Tool." .. mode .. ".listname"
 TOOL.ConfigName		= ""
 TOOL.ClientConVar[ "max_health" ] = "100"
 TOOL.ClientConVar[ "health" ] = "100"
@@ -16,21 +16,20 @@ TOOL.Information = {
 	{ name = "reload" },
 }
 
-if SERVER then 
-	local flags = { FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED }
+if SERVER then
+	local flags = bit.bor( FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED )
 	CreateConVar( "sv_adv_health_tool_nodmgforce", 1, flags, "Nullified damage will also lose its force.", 0, 1 )
 	flags = nil
-
 else
 
-	local t = "tool."..mode.."."
-	language.Add( t.."listname",	"Advanced Health" )
-	language.Add( t.."name",		"Advanced Health Tool" )
-	language.Add( t.."desc",		"Change the health-related properties of entities." )
-	language.Add( t.."0",			"Press "..(input.LookupBinding( "+speed" ) or "sprint key").." to target all constrained entities" )
-	language.Add( t.."left",		"Apply settings" )
-	language.Add( t.."right",		"Copy settings" )
-	language.Add( t.."reload",		"Reset entity settings" )
+	local t = "tool." .. mode .. "."
+	language.Add( t .. "listname",	"Advanced Health" )
+	language.Add( t .. "name",		"Advanced Health Tool" )
+	language.Add( t .. "desc",		"Change the health-related properties of entities." )
+	language.Add( t .. "0",			"Press " .. (input.LookupBinding( "+speed" ) or "sprint key") .. " to target all constrained entities" )
+	language.Add( t .. "left",		"Apply settings" )
+	language.Add( t .. "right",		"Copy settings" )
+	language.Add( t .. "reload",		"Reset entity settings" )
 
 
 	local k1, k2 = "name", "flag"
@@ -98,7 +97,7 @@ function AHT_ApplySettings( ply, ent, data, do_undo, undo_text )
 
 	local legacyUnbreak = ent.EntityMods and ent.EntityMods.Unbreakable
 	if legacyUnbreak then
-		if data.getLegacy then 
+		if data.getLegacy then
 			data[k1] = data[k1] or legacyUnbreak and legacyUnbreak.On
 		else
 			if legacyUnbreak then ent.EntityMods.Unbreakable.On = data[k1] end
@@ -117,20 +116,20 @@ function AHT_ApplySettings( ply, ent, data, do_undo, undo_text )
 
 	AHT_ApplyNRemember( ent, data.health, ent.SetHealth, ent.Health, "aht_orig_health" )
 	AHT_ApplyNRemember( ent, data.max_health, ent.SetMaxHealth, ent.GetMaxHealth, "aht_orig_max_health" )
-	ent["aht_"..k1] = data[k1] or nil
-	ent["aht_"..k2] = data[k2] ~= 0 and data[k2] or nil
+	ent["aht_" .. k1] = data[k1] or nil
+	ent["aht_" .. k2] = data[k2] ~= 0 and data[k2] or nil
 	ent.aht_damage_filtered = data[k1] or data[k2] ~= 0 or nil
-	-- m_takedamage info here: https://developer.valvesoftware.com/wiki/CBaseEntity
-	local m_takedamage = data[k1] and ( nodmgforce:GetBool() and 0 or 1 ) or ent["aht_orig_"..k3]
-	AHT_ApplyNRemember( ent, m_takedamage, function( ent, v ) ent:SetSaveValue( k3, v ) end, function( ent ) return ent:GetInternalVariable( k3 ) end, "aht_orig_"..k3 )
+	-- m_takedamage info here: https: /  / developer.valvesoftware.com / wiki / CBaseEntity
+	local m_takedamage = data[k1] and ( nodmgforce:GetBool() and 0 or 1 ) or ent["aht_orig_" .. k3]
+	AHT_ApplyNRemember( ent, m_takedamage, function( e, v ) e:SetSaveValue( k3, v ) end, function( e ) return e:GetInternalVariable( k3 ) end, "aht_orig_" .. k3 )
 
 	if SERVER then
 		data.getLegacy = true -- for (one-way) compability with the other addon
 		duplicator.StoreEntityModifier( ent, "adv_health_tool", data )
 		if do_undo then
 			undo_text = undo_text or "Set health settings"
-			undo.Create( undo_text.." ("..( ent:GetModel() or "?" )..")" )
-				undo.AddFunction( function( undo )
+			undo.Create( undo_text .. " (" .. ( ent:GetModel() or "?" ) .. ")" )
+				undo.AddFunction( function()
 					if not IsValid( ent ) then return false end
 					AHT_ApplySettings( ply, ent, oldData, false )
 				end )
@@ -162,11 +161,11 @@ if SERVER then
 		if not self:GetClientBool( "tooltip_enabled" ) then return end
 
 		local ent = self:GetOwner():GetEyeTrace().Entity
-		if not IsValid( ent ) then return end 
+		if not IsValid( ent ) then return end
 
 		for k, v in pairs( AHT_CopySettings( ent ) ) do
 			local funcKey = isbool( v ) and "SetNW2Bool" or "SetNW2Int"
-			ent[ funcKey ]( ent, "aht_"..k, v )
+			ent[ funcKey ]( ent, "aht_" .. k, v )
 		end
 
 	end
@@ -174,11 +173,11 @@ end
 
 
 function TOOL:LeftClick( trace )
-	
+
 	local ent = trace.Entity
 	if not ent or not ent:IsValid() or ent:IsWorld() or ent:IsPlayer() then return false end
 	local ply = self:GetOwner()
-	
+
 	local data = {
 		max_health	= self:GetClientNumber( "max_health" ),
 		immune_mask	= self:GetClientNumber( "immune_mask" ),
@@ -186,12 +185,12 @@ function TOOL:LeftClick( trace )
 	}
 	data.health = self:GetClientBool( "use_max" ) and data.max_health or self:GetClientNumber( "health" )
 
-	local multi = self:GetOwner():KeyDown( IN_SPEED )
+	local multi = ply:KeyDown( IN_SPEED )
 	local getter = multi and constraint.GetAllConstrainedEntities
 	local targets = getter and getter( ent ) or { [ ent ] = ent }
 	local do_undo = #targets <= 1
 	for target in pairs( targets ) do
-		AHT_ApplySettings( self:GetOwner(), target, data, do_undo )
+		AHT_ApplySettings( ply, target, data, do_undo )
 	end
 
 	return true
@@ -200,13 +199,13 @@ end
 
 
 function TOOL:RightClick( trace )
-	
+
 	local ent = trace.Entity
 	if ent:IsWorld() or not ent:IsValid() then return false end
 
 	for k, v in pairs( AHT_CopySettings( ent ) ) do
 		if isbool( v ) then v = v and 1 or 0 end
-		RunConsoleCommand( mode.."_"..k, v )
+		RunConsoleCommand( mode .. "_" .. k, v )
 	end
 	return true
 end
@@ -224,7 +223,7 @@ function TOOL:Reload( trace )
 	local getter = multi and constraint.GetAllConstrainedEntities
 	local targets = getter and getter( ent ) or { [ ent ] = ent }
 	local do_undo = #targets <= 1
-	
+
 	local data = {
 		unbreakable = false,
 		immune_mask = 0,
@@ -255,43 +254,43 @@ if CLIENT then
 
 		if not IsValid( ent ) then return end
 
-		local vecmin, vecmax = ent:WorldSpaceAABB()
+		local vecmax = select( 2, ent:WorldSpaceAABB() )
 		local pos = ent:WorldSpaceCenter()
 		pos.z = vecmax.z
 		pos = pos:ToScreen()
 		local x, y = pos.x, pos.y
-		
+
 		local health	 = ent:GetNW2Int( "aht_health" )
 		local max_health = ent:GetNW2Int( "aht_max_health" )
 		local unbreakable	 = ent:GetNW2Bool( "aht_unbreakable" )
 		local immune_mask	 = ent:GetNW2Int( "aht_immune_mask" ) or 0
 		local prop = health / max_health or 1
 
-		local text1 = ( "Health: %s / %s" ):format( health or "N/A", max_health or "N/A" )
-		local text2 = max_health == 0 and "" or ( " (%s%%)" ):format( math.Round( prop*100, 2 ) )
+		local text1 = ( "Health: %s / %s" ):format( health or "N / A", max_health or "N / A" )
+		local text2 = max_health == 0 and "" or ( " (%s%%)" ):format( math.Round( prop * 100, 2 ) )
 		local text3 = unbreakable and "Unbreakable" or ""
-		local text3 = ( text3 ~= "" and immune_mask ~= 0 ) and text3..", " or text3
-		if immune_mask ~= 0 then text3 = text3.."DMG Mask: "..immune_mask end
+		text3 = ( text3 ~= "" and immune_mask ~= 0 ) and text3 .. ", " or text3
+		if immune_mask ~= 0 then text3 = text3 .. "DMG Mask: " .. immune_mask end
 
 		prop = math.Clamp( prop, 0, 3 )
-		local txcol	= HSVToColor( 100*prop, 0.65, 0.9 )
-		local bgcol = HSVToColor( 100*prop, 1, 0.1 )
+		local txcol	= HSVToColor( 100 * prop, 0.65, 0.9 )
+		local bgcol = HSVToColor( 100 * prop, 1, 0.1 )
 			bgcol.a = 220
 		local rad   = 8
 
 		local font = "GModWorldtip"
 		surface.SetFont( font )
 		local tw1, th1 = surface.GetTextSize( text1 )
-		local tw2, th2 = surface.GetTextSize( text2 )
+		local tw2 = surface.GetTextSize( text2 )
 		local tw3, th3 = surface.GetTextSize( text3 )
 		if tw3 == 0 then th3 = 0 end
 		local tw, th = math.max( tw1 + tw2, tw3 ), th1 + th3
 
 		y = y - th
 
-		draw.RoundedBox( rad, x - tw/2 - 10, y - th1/2 - 2, tw + 20, th + 4, bgcol )
-		draw.SimpleText( text1, font, x - tw2/2, y, color_white, 1, 1 )
-		draw.SimpleText( text2, font, x + tw1/2, y, txcol, 1, 1 )
+		draw.RoundedBox( rad, x - tw / 2 - 10, y - th1 / 2 - 2, tw + 20, th + 4, bgcol )
+		draw.SimpleText( text1, font, x - tw2 / 2, y, color_white, 1, 1 )
+		draw.SimpleText( text2, font, x + tw1 / 2, y, txcol, 1, 1 )
 		if text3 ~= "" then draw.SimpleText( text3, font, x, y + th1, color_gold, 1, 1 ) end
 	end
 
@@ -315,7 +314,7 @@ function TOOL.BuildCPanel( cPanel )
 		draw.RoundedBoxEx( 8, 0, topHeight, w, h - topHeight + 5, bgcol, false, false, true, true )
 	end
 
-	cPanel:Help( "#tool."..mode..".desc" )
+	cPanel:Help( "#tool." .. mode .. ".desc" )
 
 	cPanel:ToolPresets( mode, cvarlist )
 
@@ -332,40 +331,40 @@ function TOOL.BuildCPanel( cPanel )
 			paint( self, w, h, color_red, color_gray )
 		end
 
-		local maxHealthSlider = healthForm:NumSlider( "Max Health", mode.."_max_health", 0, 5000, 0 )
-			maxHealthSlider:SetToolTip("Sets the entity's maximum health. NPCs can heal up to this amount.")
+		local maxHealthSlider = healthForm:NumSlider( "Max Health", mode .. "_max_health", 0, 5000, 0 )
+			maxHealthSlider:SetTooltip("Sets the entity's maximum health. NPCs can heal up to this amount.")
 			healthForm:ControlHelp( "I recommend using a value greather than that of Base Health to prevent weird behavior for NPCs." )
 
-		local healthSlider = healthForm:NumSlider( "Base Health", mode.."_health", 0, maxHealthSlider:GetMax(), 0 )
-			healthSlider:SetToolTip("Sets the current and duped health of the entity.")
+		local healthSlider = healthForm:NumSlider( "Base Health", mode .. "_health", 0, maxHealthSlider:GetMax(), 0 )
+			healthSlider:SetTooltip("Sets the current and duped health of the entity.")
 
 		healthForm:ControlHelp( ("You can set these higher than %s if you want."):format( maxHealthSlider:GetMax() ) )
 
 		local FR_button = vgui.Create( "DButton" )
 			FR_button:Dock( TOP )
-			FR_button:SetText( "Make fragile" )	
-			FR_button:SetImage( "icon32/hand_property.png" )
+			FR_button:SetText( "Make fragile" )
+			FR_button:SetImage( "icon32 / hand_property.png" )
 			function FR_button:DoClick()
 				maxHealthSlider.Scratch:SetValue( lowHealth )
 				healthSlider.Scratch:SetValue( lowHealth )
 			end
-			FR_button:SetToolTip( ("Set Base Health and Max Health to %s."):format( lowHealth ) )
+			FR_button:SetTooltip( ("Set Base Health and Max Health to %s."):format( lowHealth ) )
 
 		local NU_button = vgui.Create( "DButton" )
 			NU_button:Dock( TOP )
-			NU_button:SetText( "Make near-unbreakable" )	
-			NU_button:SetImage( "icon32/tool.png" )
+			NU_button:SetText( "Make near-unbreakable" )
+			NU_button:SetImage( "icon32 / tool.png" )
 			function NU_button:DoClick()
 				maxHealthSlider.Scratch:SetValue( limitHealth )
 				healthSlider.Scratch:SetValue( limitHealth )
 			end
 			NU_button:DockMargin( 15, 0, 0, 0 )
-			NU_button:SetToolTip( ("Set Base Health and Max Health to %s."):format( limitHealth ) )
-		
+			NU_button:SetTooltip( ( "Set Base Health and Max Health to %s." ):format( limitHealth ) )
+
 		healthForm:AddItem( FR_button, NU_button )
 
-		local STMH_checkBox = healthForm:CheckBox( "Heal", mode.."_use_max" )
-			STMH_checkBox:SetToolTip( ( "Set Base Health to the same value as Max Health" ) )
+		local STMH_checkBox = healthForm:CheckBox( "Heal", mode .. "_use_max" )
+			STMH_checkBox:SetTooltip( "Set Base Health to the same value as Max Health" )
 			function STMH_checkBox:OnChange( checked )
 				healthSlider:SetEnabled( not checked )
 				if checked then healthSlider.Scratch:SetValue( maxHealthSlider.Scratch:GetFloatValue() ) end
@@ -387,14 +386,14 @@ function TOOL.BuildCPanel( cPanel )
 			paint( self, w, h, color_orange, color_gray )
 		end
 
-		local ubCheckBox = filterForm:CheckBox( "Unbreakable", mode.."_unbreakable" )
-			ubCheckBox:SetToolTip( "Make the entity unable to take damage of any kind." )
+		local ubCheckBox = filterForm:CheckBox( "Unbreakable", mode .. "_unbreakable" )
+			ubCheckBox:SetTooltip( "Make the entity unable to take damage of any kind." )
 
 		filterForm:Help("Below you can choose and combine 32 types of damage to ignore.")
 		filterForm:ControlHelp("You can get a better understanding of damage types by checking the wiki link in the 'Help' section at the bottom.")
 
 
-		local cVarName = mode.."_immune_mask"
+		local cVarName = mode .. "_immune_mask"
 
 		local filterComboBox = filterForm:ComboBox( "Easy Presets", cVarName )
 			filterComboBox:Dock( TOP )
@@ -415,27 +414,27 @@ function TOOL.BuildCPanel( cPanel )
 
 		-- Get current ConVar value (default to 0 if invalid)
 		local currentMask = tonumber( cVar:GetString() ) or 0
-		
+
 		for _, dmgType in ipairs( dmgEnums ) do
 			-- Create checkbox with damage type name
 			local checkbox = vgui.Create( "DCheckBoxLabel", filterForm )
 			checkbox:SetDark( true )
 			checkbox:SetText( dmgType.name )
 			checkbox:SetValue( bit.band( currentMask, dmgType.flag ) ~= 0 )
-			
+
 			-- Update ConVar on change using bitwise operations
 			function checkbox:OnChange( checked )
 
 				if syncing then return end
 
 				local newMask = tonumber( cVar:GetString() ) or 0 -- do NOT use cVar:GetInt() as it returns a rounded value for large ints! (e.g 33 554 431 -> 33 554 432)
-				
+
 				if checked then
 					newMask = bit.bor( newMask, dmgType.flag )
 				else
 					newMask = bit.band( newMask, bit.bnot( dmgType.flag ) )
 				end
-				
+
 				RunConsoleCommand( cVarName, newMask )
 
 			end
@@ -475,18 +474,18 @@ function TOOL.BuildCPanel( cPanel )
 			paint( self, w, h, color_green, color_gray )
 		end
 
-		local tooltipCheckBox = helpForm:CheckBox( "Enable Tooltips", mode.."_tooltip_enabled" )
-			tooltipCheckBox:SetToolTip( "Show a health tooltip when looking at something with this tool." )
+		local tooltipCheckBox = helpForm:CheckBox( "Enable Tooltips", mode .. "_tooltip_enabled" )
+			tooltipCheckBox:SetTooltip( "Show a health tooltip when looking at something with this tool." )
 
 		local valveButton, fpButton = vgui.Create( "DButton", helpForm ), vgui.Create( "DButton", helpForm )
 			valveButton:SetText( "Damage types (Valve Wiki)" )
 			fpButton:SetText( "Damage types (Facepunch Wiki)" )
-			
-			valveButton:SetImage( "games/16/hl2.png" )
-			fpButton:SetImage( "games/16/garrysmod.png" )
-			
-			function valveButton:DoClick() gui.OpenURL( "https://developer.valvesoftware.com/wiki/Damage_types" ) end
-			function fpButton:DoClick() gui.OpenURL( "https://wiki.facepunch.com/gmod/Enums/DMG" ) end
+
+			valveButton:SetImage( "games / 16 / hl2.png" )
+			fpButton:SetImage( "games / 16 / garrysmod.png" )
+
+			function valveButton:DoClick() gui.OpenURL( "https: /  / developer.valvesoftware.com / wiki / Damage_types" ) end
+			function fpButton:DoClick() gui.OpenURL( "https: /  / wiki.facepunch.com / gmod / Enums / DMG" ) end
 
 			helpForm:AddItem( fpButton )
 			helpForm:AddItem( valveButton )
